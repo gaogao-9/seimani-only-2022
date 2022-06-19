6810407dd250ecfffe890a5a64f06ba69a04f8a3
gaogao-9 <gaotuki@gmail.com>
2022-06-19 22:29:30 +0900

    fix: 初期を白背景に変更

--- components/layouts/AnimationLayout.tsx
+++ components/layouts/AnimationLayout.tsx
@@ -8,7 +8,7 @@ const Wrapper = styled.div`
   width: 100vw;
   height: 100vh;
   overflow: hidden;
-  background-color: var(--chakra-colors-black-full);
+  background-color: var(--chakra-colors-white-full);
 `;
 
 const Contents = styled.div`
